1d429ecf65e00c76afe0481b6e9311a3054d3fe7
Tim Wilson <twilsonco@gmail.com>
2025-07-14 16:15:07 -0600

    same with DEBUG logging

--- src/functions.py
+++ src/functions.py
@@ -69,7 +69,7 @@ def logger(message: str, log_type=0):
         output = f"[INFO]: {output}"
     elif log_type == 2:
         output = f"[ERROR]: {output}"
-    elif log_type == 3 and (debug and debug_level == "debug"):
+    elif log_type == 3 and (debug_level == "debug"):
         output = f"[DEBUG]: {output}"
     elif log_type == 4:
         output = f"[WARNING]: {output}"
